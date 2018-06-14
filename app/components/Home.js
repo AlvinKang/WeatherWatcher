e4ebb2b2897f816ec9430b4d824561d235016265
Alvin Kang <oneinallin1@gmail.com>
2018-06-14 14:03:55 -0700

    Hook up submit button to print input state

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,14 +1,45 @@
 const React = require("react");
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchValue: ""
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  // Send API request and route to other page
+  handleSubmit(event) {
+    // Prevent default submit
+    event.preventDefault();
+    // TODO: Call API, route to next page
+    console.log(this.state.searchValue);
+  }
+
+  //
+  handleChange(event) {
+    const searchValue = event.target.value;
+    this.setState({
+      searchValue: searchValue
+    });
+  }
+
   render() {
     return (
       <div className="home-container">
-        <form className="column">
+        <form className="column" onSubmit={this.handleSubmit}>
           <label className="header" htmlFor="search-box">
             Enter a City and State
           </label>
-          <input id="search-box" type="text" placeholder="Las Vegas, Nevada" />
+          <input
+            id="search-box"
+            type="text"
+            placeholder="Las Vegas, Nevada"
+            onChange={this.handleChange}
+            value={this.state.searchValue}
+          />
           <button className="button" type="submit">
             Get Weather
           </button>
--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,19 +1,55 @@
 const React = require("react");
 
-function Nav(props) {
-  return (
-    <div className="nav">
-      <p className="app-header">
-        <i className="fas fa-sun" />
-        <span id="app-name"> Weather Watcher</span>
-      </p>
+class Nav extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchValue: ""
+    };
 
-      <form className="nav-search">
-        <input type="text" placeholder="Las Vegas, Nevada" />
-        <button className="button">Get Weather</button>
-      </form>
-    </div>
-  );
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const searchValue = event.target.value;
+    this.setState({
+      searchValue: searchValue
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    // TODO: Call API, route to next page
+    console.log(this.state.searchValue);
+  }
+
+  render() {
+    return (
+      <div className="nav">
+        <p className="app-header">
+          <i className="fas fa-sun" />
+          <span id="app-name"> Weather Watcher</span>
+        </p>
+
+        <form className="nav-search" onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Las Vegas, Nevada"
+            onChange={this.handleChange}
+            value={this.state.searchValue}
+          />
+          <button
+            className="button"
+            type="submit"
+            disabled={!this.state.searchValue}
+          >
+            Get Weather
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 module.exports = Nav;
