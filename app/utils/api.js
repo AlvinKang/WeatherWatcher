7618a67b6767ba266ec5d8d974fecac1b4446693
Alvin Kang <oneinallin1@gmail.com>
2018-06-19 15:44:30 -0700

    Add error logic

--- app/utils/api.js
+++ app/utils/api.js
@@ -16,7 +16,24 @@ function getCurrentWeather(location) {
   return axios
     .get(url)
     .then(res => res.data)
-    .catch(err => handleError);
+    .catch(error => {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+    });
 }
 
 // 5-day forecast
