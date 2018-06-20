7d211b938cf8ca4b82766c4c9ec8f7c700c4b074
Alvin Kang <oneinallin1@gmail.com>
2018-06-19 20:01:11 -0700

    Remove nav search

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -12,67 +12,12 @@ const NavHeader = () => {
   );
 };
 
-class Nav extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchValue: "",
-      fireRedirect: false
-    };
-
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    this.setState({
-      fireRedirect: true
-    });
-  }
-
-  handleChange(event) {
-    const searchValue = event.target.value;
-    this.setState({
-      searchValue: searchValue,
-      fireRedirect: false
-    });
-  }
-
-  render() {
-    const { fireRedirect } = this.state;
-
-    return (
-      <div className="nav">
-        <NavHeader />
-        <form className="nav-search" onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            placeholder="Las Vegas, Nevada"
-            onChange={this.handleChange}
-            value={this.state.searchValue}
-          />
-          <button
-            className="button"
-            type="submit"
-            disabled={!this.state.searchValue}
-          >
-            Get Weather
-          </button>
-        </form>
-        {fireRedirect && (
-          <Redirect
-            push
-            to={{
-              pathname: "/forecast",
-              search: `?location=${this.state.searchValue}`
-            }}
-            from="/"
-          />
-        )}
-      </div>
-    );
-  }
-}
+const Nav = () => {
+  return (
+    <div className="nav">
+      <NavHeader />
+    </div>
+  );
+};
 
 module.exports = Nav;
